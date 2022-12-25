7431d0936a2dabd22f07beda2bb145ddd6089366
AnDX <andxh58chn@gmail.com>
2022-12-25 22:03:20 +0700

    feat: deducation contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const secret = require("./secret.json");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: secret.url, // endpoint alchemy
+      accounts: [secret.private_key], // private key metamask wallet
+    },
+  },
 };
